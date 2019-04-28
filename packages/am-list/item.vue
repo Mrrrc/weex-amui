ee04d327656fd2937c07c62181bf5427af0dc334
Mrrrc <30523768+Mrrrc@users.noreply.github.com>
2019-04-28 16:06:01 +0800

    Update item.vue

--- packages/am-list/item.vue
+++ packages/am-list/item.vue
@@ -10,8 +10,10 @@
     </slot>
     <div class="am-list-line" :class="lineClass">
       <div v-if="!!title" :class="['am-list-content', `am-list-content-text-${titleNumber}`]">
-        <text class="am-list-content-text" :class="contentTextClass">{{title}}</text>
-        <text v-if="true" class="am-list-title-required">*</text>
+        <div class="am-list-title">
+	<text class="am-list-content-text" :class="contentTextClass">{{title}}</text>
+    <text v-if="required" class="am-list-title-required">*</text>
+</div>
         <slot name="brief">
           <text class="am-list-content-brief" v-if="brief">{{brief}}</text>
         </slot>
